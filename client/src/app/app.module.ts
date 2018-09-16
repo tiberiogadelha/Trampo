9e358721ea9368a30cd1815552791ef19731318a
Ronan Souza <ronan.souza@ccc.ufcg.edu.br>
2018-09-16 14:16:58 -0300

    Criação da tela de cadastro de cliente (ainda imcompleta)

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,20 +6,27 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { CadastroClientePage } from '../pages/cadastro-cliente/cadastro-cliente';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroClientePage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp, {
+      statusbarPadding: true,},
+      { links: [
+        { component: CadastroClientePage, name:'CadastroClientePage', segment: 'cadastro-cliente'}
+      ]})
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    CadastroClientePage
   ],
   providers: [
     StatusBar,
