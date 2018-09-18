953c61b10de6523f090fee3f1ff44dce3a7dc47b
Ronan Souza <ronan.souza@ccc.ufcg.edu.br>
2018-09-17 23:25:51 -0300

    Adiciona tela de cadastro de fornecedor

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,26 +7,30 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { CadastroClientePage } from '../pages/cadastro-cliente/cadastro-cliente';
+import { CadastroFornecedorPage } from '../pages/cadastro-fornecedor/cadastro-fornecedor';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    CadastroClientePage
+    CadastroClientePage,
+    CadastroFornecedorPage
   ],
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp, {
       statusbarPadding: true,},
       { links: [
-        { component: CadastroClientePage, name:'CadastroClientePage', segment: 'cadastro-cliente'}
+        { component: CadastroClientePage, name:'CadastroClientePage', segment: 'cadastro-cliente'},
+        { component: CadastroFornecedorPage, name:'CadastroFornecedorPage', segment: 'cadastro-fornecedor'}
       ]})
   ],
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
     HomePage,
-    CadastroClientePage
+    CadastroClientePage,
+    CadastroFornecedorPage
   ],
   providers: [
     StatusBar,
---
+++ client/src/pages/cadastro-fornecedor/cadastro-fornecedor.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the CadastroFornecedorPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-cadastro-fornecedor',
+  templateUrl: 'cadastro-fornecedor.html',
+})
+export class CadastroFornecedorPage {
+
+  public anArray:any=[];
+  public data:boolean;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  goTo(){
+    console.log('this.anArray',this.anArray);
+    this.data=true;
+  }
+  Add(){
+   this.anArray.push({'value':''});
+  }
+
+}
